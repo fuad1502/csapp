56f722579a2d7fd1391c7a026ec483692b527286
Fuad Ismail <fuad1502@gmail.com>
2024-01-08 15:13:25 +0700

    Use rdtsc instead of clock to calculate number of clock cycles

--- chapter-examples/chapter5/vector/main.c
+++ chapter-examples/chapter5/vector/main.c
@@ -1,8 +1,9 @@
 #include "operation.h"
 #include "vector.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <x86intrin.h>
 
 #define MAX_N 1000000
 #define STEP_N 10000
@@ -11,6 +12,12 @@
 typedef void (*combine_f)(vec_ptr, data_t *);
 combine_f f[F_NUM] = {&combine1, &combine2, &combine3, &combine4};
 char *f_name[F_NUM] = {"combine1", "combine2", "combine3", "combine4"};
+
+uint64_t rdtsc() {
+  unsigned int lo, hi;
+  __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+  return ((uint64_t)hi << 32) | lo;
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -41,10 +48,10 @@ int main(int argc, char *argv[]) {
     // Benchmark all functions
     for (int i = 0; i < F_NUM; i++) {
       data_t data;
-      clock_t begin = clock();
+      uint64_t begin = rdtsc();
       f[i](v, &data);
-      clock_t end = clock();
-      long duration = (end - begin);
+      uint64_t end = rdtsc();
+      uint64_t duration = (end - begin);
 
       // Write duration to output file
       fprintf(fp, "%ld, ", duration);
